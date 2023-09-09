159a64b9b2f9fccdfb914525da0c7fd2302f12b7
kennedyuc <chukwukakennedyuche@gmail.com>
2023-09-09 15:05:07 +0100

    updated the project script

---
+++ Makefile
@@ -0,0 +1,30 @@
+ENV :=dev
+SHELL := /bin/bash
+
+CWD:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+
+DOCKER_REPO ?=
+DOCKER_PASSWORD ?=
+IMAGE_TAG ?= latest
+
+.PHONY: docker-login
+docker-login:
+	echo $(DOCKER_PASSWORD) | docker login -u $(DOCKER_REPO) --password-stdin
+
+.PHONY: skaffold-build-local
+skaffold-build-local:
+	skaffold build  --platform linux/amd64 --default-repo=$(DOCKER_REPO)
+
+.PHONY: skaffold-build
+skaffold-build:
+	skaffold build  --platform linux/amd64 --default-repo=$(DOCKER_REPO) --push
+
+.PHONY: docker-build-backend
+docker-build-backend:
+	docker build backend -t $(DOCKER_REPO)/api:$(IMAGE_TAG) -f containers/Dockerfile.api
+	docker push $(DOCKER_REPO)/api:$(IMAGE_TAG)
+
+.PHONY: docker-build-frontend
+docker-build-frontend:
+	docker build frontend -t "$(DOCKER_REPO)/$(IMAGE_TAG)" -f containers/Dockerfile.web
+	docker push "$(DOCKER_REPO)/web:$(IMAGE_TAG)"
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from 'App'
+import store from 'store'
+
+import 'index.css'
+import { setAuthorized } from 'store/slices/authSlice'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+store.dispatch(setAuthorized())
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+)
